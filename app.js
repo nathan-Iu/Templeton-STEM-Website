0a2088f471a3e9fd006c8d40027c697ba8d0e4e9
Nathan Lu <nathanlu_@hotmail.com>
2019-04-19 17:22:29 -0700

    Added keywords

--- app.js
+++ app.js
@@ -91,6 +91,6 @@ app.post("/send/:id", function (req, res) {
     })
 })
 
-app.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started");
 });
